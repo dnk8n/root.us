f81c8a288a7ce15773a8e665c60b33fe2d6b0319
Christo Goosen <christo.goosen@takectrl.co.za>
2018-07-21 15:36:25 +0200

    Welcome endpoint

--- root_us/api.py
+++ root_us/api.py
@@ -1,14 +1,20 @@
 from flask import Flask
-
-# from flask import app
-
+# from root_us.rehive_client import rehive_client
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {
+    response = {
         "message": "Welcome to the Root.us API"
     }
+    return app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+    # return "Welcome to the root.us APIs"
+
 
 @app.route("/webhook")
 def dialogflow_webhook(request):
